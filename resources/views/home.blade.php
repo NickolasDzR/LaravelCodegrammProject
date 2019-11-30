714ff5d53dde92b6db6129470aab9395d49a475d
NickolasDzR <nikolyae@mail.ru>
2019-11-30 19:32:52 +0300

    laravelCodegrammProject ;

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row p-5">
+        <div class="col-3"><img src="/img/logo.png" alt="logo"></div>
+        <div class="col-9 pt-5">
+        <h1>Codegramm</h1>
+        <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>123</strong> following</div></div>
+            <div class="pt-3 font-weight-bold">codegramm</div>
+            <div class="pt-1">We're nice site of your the best photos of your life that you can easily to load up into our codegram and can be not worry about safety this ones. </div>
+            <div class=""><a href="#">www.nonexistent.io</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/78722084_1002073883477295_2337142880813018505_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=103&oh=9d8baed741327f0dd08ba6d6b2204d8c&oe=5E7046B4" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/74527631_1102220986648257_8836227055142371906_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=109&oh=0af8a08f1e94536504f5f850d5935c18&oe=5E6D8EB7" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/e35/77407435_473851199908210_1426864340016628332_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&oh=45df1d9ec8c9249265d5bccf66f1c0ba&oe=5E842B67" class="w-100" alt="image1">
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+        <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/e35/77407435_473851199908210_1426864340016628332_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&oh=45df1d9ec8c9249265d5bccf66f1c0ba&oe=5E842B67" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/74527631_1102220986648257_8836227055142371906_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=109&oh=0af8a08f1e94536504f5f850d5935c18&oe=5E6D8EB7" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+        <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/78722084_1002073883477295_2337142880813018505_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=103&oh=9d8baed741327f0dd08ba6d6b2204d8c&oe=5E7046B4" class="w-100" alt="image1">
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/78722084_1002073883477295_2337142880813018505_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=103&oh=9d8baed741327f0dd08ba6d6b2204d8c&oe=5E7046B4" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/74527631_1102220986648257_8836227055142371906_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=109&oh=0af8a08f1e94536504f5f850d5935c18&oe=5E6D8EB7" class="w-100" alt="image1">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/e35/77407435_473851199908210_1426864340016628332_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&oh=45df1d9ec8c9249265d5bccf66f1c0ba&oe=5E842B67" class="w-100" alt="image1">
+        </div>
+    </div>
+</div>
+@endsection
+
+
